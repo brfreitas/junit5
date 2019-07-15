7f0156642cbd22bd1d357035f9cb2517275b8f7f
Bruno Freitas <bruno.freitas@ifood.com.br>
2019-07-14 21:10:39 -0300

    Create JUnit5Test class and implements some tests on JUnit5 structure

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class JUnit5Test {
+
+    @BeforeAll
+    static void inicializarRecursosExternos() {
+        System.out.println("Inicializando os recursos externos...");
+    }
+
+    @BeforeEach
+    void inicializarObjetosMock() {
+        System.out.println("Inicializando objetos mock...");
+    }
+
+    @Test
+    void algumTeste() {
+        System.out.println("Executando algum teste...");
+        assertTrue(true);
+    }
+
+    @Test
+    void outroTeste() {
+        assumeTrue(true);
+
+        System.out.println("Executando outro teste...");
+        assertNotEquals(1, 42, "Porque estes valores não são iguais?");
+    }
+
+    @Test
+    @Disabled
+    void disabilitarTeste() {
+        System.exit(1);
+    }
+
+    @AfterEach
+    void parando() {
+        System.out.println("Parando...");
+    }
+
+    @AfterAll
+    static void liberarRecursosExternos() {
+        System.out.println("Liberando os recursos externos...");
+    }
 }
